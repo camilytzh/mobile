501db3d7c6ba2c6cd378469b99d589c8ac8e2340
Tania Camily Torres Zhunio <126212496+camilytzh@users.noreply.github.com>
2026-08-05 02:55:49 +0000

    registro de gastos

--- src/pages/AddExpense.tsx
+++ src/pages/AddExpense.tsx
@@ -1,2 +1,43 @@
-export default function addExpenses() {
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonSelect,
+    IonSelectOption,
+    IonButton
+} from '@ionic/react';
+export default function AddExpense() {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Registrar gasto</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className='ion-padding'>
+                <IonItem>
+                    <IonLabel position='stacked'>Descripción</IonLabel>
+                    <IonInput placeholder='Ej: Almuerzo' />
+                </IonItem>
+                <IonItem>
+                    <IonLabel position='stacked'>Monto</IonLabel>
+                    <IonInput type='number' placeholder='0.00' />
+                </IonItem>
+                <IonItem>
+                    <IonLabel position='stacked'>Categoría</IonLabel>
+                    <IonSelect placeholder='Seleccione'>
+                        <IonSelectOption value='comida'>Comida</IonSelectOption>
+                        <IonSelectOption value='transporte'>Transporte</IonSelectOption>
+                        <IonSelectOption value='salud'>Salud</IonSelectOption>
+                        <IonSelectOption value='ocio'>Ocio</IonSelectOption>
+                    </IonSelect>
+                </IonItem>
+                <IonButton expand='block' className='ion-margin-top'> Guardar </IonButton>
+            </IonContent>
+        </IonPage>);
 }
